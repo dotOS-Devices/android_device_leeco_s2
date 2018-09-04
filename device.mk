fa5a90d31f79f57af0260f940b1f49dfed18ea1c
Akash Mondal <a98mondal@gmail.com>
2018-09-04 03:14:48 +0000

    Revert "s2: Enforce RRO on framework-res"
    This reverts commit c89c6ea3cabfe43ee65840aa1c0adda448b69c03.

--- device.mk
+++ device.mk
@@ -26,9 +26,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aosp
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
-
 # Screen density
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_AAPT_CONFIG := normal
